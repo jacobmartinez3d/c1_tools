5aad604327f1882ec89f0869a541acd5f40dbf5d
Jacob Martinez <jm@conditionone.com>
2017-05-22 12:04:31 -0700

    turning submitShot() into class, and updating any associated code

--- c1_tools/c1_tools.py
+++ c1_tools/c1_tools.py
@@ -1,12 +1,14 @@
 import nuke
 import nukescripts
-from nukescripts import panels
+#from nukescripts import panels
 from PySide import QtGui
 import os
 import re
 from string import ascii_lowercase
 import shutil
 import threading
+
+from submitShot import submitShotDialogue
 
 c1_folders = {
 '___CameraRaw': False,
@@ -170,101 +172,6 @@ def versionUp(file):
 
     nuke.message('Current version: ' + newVersion)
 
-def submitShot(file):
-    #_NOTES_____________________________________________________________________
-    # -error when file not named
-    # -currently creates folder on gladiator before user has chance to accept or not
-    #___________________________________________________________________________
-    filepath = os.path.abspath(file)
-    # filename pieces
-    filename = os.path.basename(file).split('_v')
-    currentVersion = int(os.path.basename(file).split('_v')[1].split('.')[0])
-
-    versionFolder = os.path.abspath(os.path.join(filepath, os.pardir))
-    localShotFolder = os.path.dirname(versionFolder)
-    shotName = os.path.basename(localShotFolder)
-    showCode = filename[0].split('_')[0]
-
-    gladiator = findGladiator()
-    def setServerShotFolder(gladiator):
-        # scan gladiator and return the remote shot folder path that matches current local shot folder name
-        serverShowFolder = None
-
-        for folder in os.listdir(gladiator):
-            # validate that it's a directory
-            if os.path.isdir(os.path.join(gladiator, folder)):
-                # check if ends with showCode
-                if folder.split('_')[-1] == showCode:
-                    serverShowFolder = os.path.join(gladiator, folder)
-        serverShotFolder = os.path.join(os.path.join(serverShowFolder, '6.VFX'), shotName) + os.sep
-        return serverShotFolder
-    serverShotFolder = setServerShotFolder(gladiator)
-    # get latest version on server
-    latestVersion = 0
-    for folder in os.listdir(serverShotFolder):
-        pieces = folder.split('_v')
-        if os.path.isdir(os.path.join(serverShotFolder, folder)):
-            # one last small validation..
-            if pieces[0].split('_')[0] == showCode and int(pieces[1]) > 0:
-                versionNum = int(pieces[1])
-                latestVersion = versionNum if versionNum > latestVersion else latestVersion
-
-    def createNewRemoteVersion(serverShotFolder, latestVersion):
-        newVersionFolderName = shotName + '_v' + str(latestVersion + 1).zfill(3)
-        newVersionFolderPath = os.path.join(serverShotFolder, newVersionFolderName)
-
-
-        localPrerenders = os.path.join(versionFolder, 'Prerenders')
-        remotePrerenders = os.path.join(newVersionFolderPath, 'Prerenders')
-
-        if len(os.listdir(localPrerenders)) < 2:
-            nuke.message('Your Prerenders folder has less than 2 files in it. Please check and re-submit!')
-            return
-
-        # create folders/files on Gladiator...
-        os.mkdir(newVersionFolderPath)
-        # do little dance to save to remote dir, then revert back to previous local session
-        currentScript = nuke.toNode('root').name()
-        nuke.scriptSaveAs(os.path.join(newVersionFolderPath, newVersionFolderName + '.nk'))
-        nuke.toNode('root').knob('name').setValue(currentScript)
-        os.mkdir(remotePrerenders)
-
-        def copyPrerenders():
-            task = nuke.ProgressTask("Submitting...")
-            files = os.listdir(localPrerenders)
-
-            progIncr = 100.0 / len(files)
-
-            for i, f in enumerate(files):
-                if task.isCancelled():
-                    nuke.executeInMainThread( nuke.message, args=( "Aborted!" ) )
-                    return;
-                task.setProgress(int(i * progIncr))
-                task.setMessage(f)
-                copyFrom = os.path.join(localPrerenders, f)
-                copyTo = os.path.join(remotePrerenders, f)
-
-                shutil.copyfile(copyFrom, copyTo)
-            nuke.executeInMainThread(nuke.message, args=('Shot succsessfully submitted to Gladiator as: ' + newVersionFolderName))
-
-        def p_submitShot():
-            p = nukescripts.PythonPanel('Submit Shot...')
-            p.addKnob(nuke.Text_Knob('','', 'Latest version of this shot on Gladiator is:\n\n' + shotName + '_v' + str(latestVersion).zfill(3) + '.\n\n'))
-            autoVersionButton = nuke.PyScript_Knob("autoVersion", newVersionFolderName)
-            p.addKnob(autoVersionButton)
-
-            def bleh():
-                nuke.message('bleergh!')
-            autoVersionButton.setCommand(bleh())
-
-            #p.addKnob(nuke.PythonKnob(filename[0]))
-
-            p.showModalDialog()
-        # if nuke.ask('Latest version of this shot on Gladiator is:\n\n' + shotName + '_v' + str(latestVersion).zfill(3) + '.\n\n' + 'Continue submission as: ' + newVersionFolderName + '?'):
-            # threading.Thread( target = copyPrerenders ).start()
-
-        p_submitShot()
-
-
-        # shutil.copytree(localPrerenders, remotePrerenders)
-    createNewRemoteVersion(serverShotFolder, latestVersion)
+def submitShot( file ):
+    p = submitShotDialogue()
+    p.showModalDialogue()
