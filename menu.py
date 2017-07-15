6c0fd5c0ff240a3a343dbda48b90a91d6e2bef58
Jacob Martinez <jm@conditionone.com>
2017-07-14 17:26:06 -0700

    including icon PSD's

--- menu.py
+++ menu.py
@@ -7,10 +7,10 @@
 # def open_shotBrowser():
 #     nukescripts.panels.registerWidgetAsPanel('c1_shotBrowser.ShotBrowser', 'C1 Shot Browser', 'c1_shotBrowser', True).addToPane(nuke.getPaneFor('Properties.1'))
 # # _callbacks____________________________________________________________________
-# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/C1 Shot Browser', 'open_shotBrowser()', icon='sb.png' )
-# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/Submit Shot', 'c1_tools.submitShot(nuke.root().name())' )
+# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/C1 Shot Browser', 'open_shotBrowser()', icon='c1vrlogo.png' )
+# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/C1 Submit Shot', 'c1_tools.submitShot(nuke.root().name())', icon='submitShot.png' )
+# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/C1 Version Up', 'c1_tools.versionUp(nuke.root().name())', icon='versionUp.png' )
 # nuke.menu( 'Nuke' ).addSeparator( index=2 )
-# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/Create Missing Subfolders', 'c1_tools.createShotFolder()' )
+# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/C1 Create Missing Subfolders', 'c1_tools.createShotFolder()')
 # nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/Scan for Missing Frames', 'c1_tools.Luis_Solver()' )
-# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/Version Up', 'c1_tools.versionUp(nuke.root().name())' )
-# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/Render OU.mp4 with FFmpeg', 'c1_tools.ffmpegRender()' )
+# nuke.menu( 'Nuke' ).addCommand( 'C1 Tools/Render OU.mp4 with FFmpeg', 'c1_tools.ffmpegRender()', icon='ffmpeg.png' )
